3baa6e7f7fd63cac8cecf37656a82b671d7f7ce9
AzizRAA <azizbekruzmatovraa@gmail.com>
2022-07-16 19:44:25 +0300

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,34 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const path = require('path');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
+ module.exports = {
+   mode: 'development',
+   entry: {
+     index: './src/index.js'
+   },
+   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
+   plugins: [
+     new HtmlWebpackPlugin({
+       title: 'Development',
+     }),
+   ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+  optimization: {
+    runtimeChunk: 'single',
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
-};
+ };
